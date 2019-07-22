3f394a207adb0f09e0e563553cef5926e0915a5a
Nicolas Silva <nical@fastmail.com>
2019-07-22 09:18:27 +0200

    Provide the vertex source to the geometry builder.

--- tessellation/src/path_fill.rs
+++ tessellation/src/path_fill.rs
@@ -1992,7 +1992,7 @@ impl MonotoneTessellator {
     }
 
     #[cfg(feature="experimental")]
-    pub(crate) fn flush_experimental(&mut self, output: &mut dyn GeometryBuilder<Point>) {
+    pub(crate) fn flush_experimental(&mut self, output: &mut dyn GeometryBuilder<crate::experimental::Vertex>) {
         for &(a, b, c) in &self.triangles {
             output.add_triangle(a, b, c);
         }
